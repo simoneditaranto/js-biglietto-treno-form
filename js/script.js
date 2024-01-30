5d2a395637c65e5787ee38b24b30f964fe4bae2a
s.ditaranto <simodita99@gmail.com>
2024-01-30 17:47:18 +0100

    milestone 2 completata

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 // memorizzo il bottone
-const buttonElement = document.querySelector("#button");
+const buttonElement = document.querySelector("#button-calc");
 
 // memorizzo i campi di input
 const inputAgeElement = document.querySelector("#userAge");
 const inputDistanceElement = document.querySelector("#userDistance");
+const userNameElement = document.querySelector("#textName");
 
 let ticketPrice = 0;
 console.log("start " + ticketPrice);
@@ -16,26 +17,29 @@ buttonElement.addEventListener("click",
 
         if(userDistance > 0 && Number(inputAgeElement.value) > 0) {
 
-            ticketPrice = userDistance * 0.21; 
+            ticketPrice = userDistance * 0.21;
+            document.getElementById("ticket-type").innerHTML = "Biglietto Standard";
     
             if(Number(inputAgeElement.value) <= 17) {
                 
                 // se l'utente è minorenne
                 ticketPrice = ticketPrice - ((ticketPrice / 100) * 20);
+                document.getElementById("ticket-type").innerHTML = "Biglietto Young";
     
             } else if(Number(inputAgeElement.value) >= 65){
     
                 // se l'utente è over 65
                 ticketPrice = ticketPrice - ((ticketPrice / 100) * 40);
+                document.getElementById("ticket-type").innerHTML = "Biglietto Over";
     
             }
             console.log("prezzo " + ticketPrice);
-            document.getElementById("price").innerHTML = `Il prezzo del tuo biglietto è € ${ticketPrice.toFixed(2)}`;
-            document.getElementById("error").innerHTML = "";
+            document.getElementById("ticket-price").innerHTML = `${ticketPrice.toFixed(2)} €`;
+            document.getElementById("userName").innerHTML = userNameElement.value;
 
         } else {
-            document.getElementById("error").innerHTML = `Valori inseriti non corretti`;
-            document.getElementById("price").innerHTML = "";
+            document.getElementById("ticket-price").innerHTML = "I valori inseriti non sono corretti";
+            document.getElementById("userName").innerHTML = " ";
         }
         
     }
