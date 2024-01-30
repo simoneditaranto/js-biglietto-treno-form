70c8353544c4ff337e2c9e4def873b9e7778fd7b
s.ditaranto <simodita99@gmail.com>
2024-01-30 16:22:19 +0100

    milestone 1 completata, controlli aggiunti

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@ const buttonElement = document.querySelector("#button");
 // memorizzo i campi di input
 const inputAgeElement = document.querySelector("#userAge");
 const inputDistanceElement = document.querySelector("#userDistance");
-console.log(inputAgeElement.value);
-
 
 let ticketPrice = 0;
 console.log("start " + ticketPrice);
@@ -13,20 +11,33 @@ console.log("start " + ticketPrice);
 // quando premo il bottone viene calcolato il prezzo del biglietto
 buttonElement.addEventListener("click",
     function() {
-        const userDistance = inputDistanceElement.value;
-        console.log("distanza: " + userDistance);
-        ticketPrice = userDistance * 0.21;
-        console.log("prezzo " + ticketPrice);
-        if(Number(inputAgeElement.value) <= 17) {
-            // se l'utente è minorenne
-            ticketPrice = ticketPrice - ((ticketPrice / 100) * 20);
-            console.log("minorenne " + ticketPrice);
-        } else if(Number(inputAgeElement.value) >= 65){
-            // se l'utente è over 65
-            ticketPrice = ticketPrice - ((ticketPrice / 100) * 40);
-            console.log("over 65 " + ticketPrice);
+
+        const userDistance = Number(inputDistanceElement.value);
+
+        if(userDistance > 0 && Number(inputAgeElement.value) > 0) {
+
+            ticketPrice = userDistance * 0.21; 
+    
+            if(Number(inputAgeElement.value) <= 17) {
+                
+                // se l'utente è minorenne
+                ticketPrice = ticketPrice - ((ticketPrice / 100) * 20);
+    
+            } else if(Number(inputAgeElement.value) >= 65){
+    
+                // se l'utente è over 65
+                ticketPrice = ticketPrice - ((ticketPrice / 100) * 40);
+    
+            }
+            console.log("prezzo " + ticketPrice);
+            document.getElementById("price").innerHTML = `Il prezzo del tuo biglietto è € ${ticketPrice.toFixed(2)}`;
+            document.getElementById("error").innerHTML = "";
+
+        } else {
+            document.getElementById("error").innerHTML = `Valori inseriti non corretti`;
+            document.getElementById("price").innerHTML = "";
         }
-        document.getElementById("price").innerHTML = `Il prezzo del tuo biglietto è € ${ticketPrice}`;
+        
     }
 )
 
